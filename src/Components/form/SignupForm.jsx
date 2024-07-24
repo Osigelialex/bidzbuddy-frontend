6d4fad19fb1ece73717b7cce463087a599917ee8
Osigelialex <osigelialex@gmail.com>
2024-07-24 01:46:50 +0100

    fix: Add error logging for test in SignupForm

--- src/Components/form/SignupForm.jsx
+++ src/Components/form/SignupForm.jsx
@@ -44,6 +44,7 @@ const Signup = () => {
       navigate("/link-sent");
       reset();
     } catch (error) {
+      console.log(error);
       if (error.response) {
         setError("root", {
           message: error.response.data.message,
