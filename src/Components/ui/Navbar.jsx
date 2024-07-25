235d6904f31935928f8ef33ad5ebe694de582548
Osigelialex <osigelialex@gmail.com>
2024-07-25 11:09:13 +0100

    chore: Update Navbar styling for better ui on smaller screens

--- src/Components/ui/Navbar.jsx
+++ src/Components/ui/Navbar.jsx
@@ -11,11 +11,11 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="sticky top-0 z-50 flex items-center justify-between bg-purple-950 text-white sm:text-black sm:bg-white px-10 py-2 align-middle sm:px-12">
+      <nav className="sticky top-0 z-50 flex items-center justify-between bg-[#301934] text-white sm:text-black sm:bg-white px-10 py-2 align-middle sm:px-12">
         <Link to="/">
-          <div className="flex cursor-pointer gap-2 text-lg font-bold text-purple-500">
+          <div className="flex cursor-pointer gap-2 text-lg font-bold sm:text-purple-500 text-white">
             <RiAuctionFill size={30} />
-            <p className="text-lg text-purple-500 font-extrabold">Bidzbuddy</p>
+            <p className="text-lg sm:text-purple-500 text-white font-extrabold">Bidzbuddy</p>
           </div>
         </Link>
 
